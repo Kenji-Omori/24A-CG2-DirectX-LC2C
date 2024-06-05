664959915e1311e4efc71dcee22fa9f6e69be270
KenjiOmori <oomoriknj@g.neec.ac.jp>
2024-06-05 17:12:23 +0900

    Texture表示できた！！！！！！！！

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,21 +1,16 @@
+#include "Object3d.hlsli"
+
 struct transformationmatrix
 {
   float32_t4x4 wvp;
 };
 ConstantBuffer<transformationmatrix> gTransformationMatrix : register(b0);
 
-
-struct VertexShaderOutput
-{
-  float32_t4 position : SV_POSITION;
-};
-
-#include "Object3d.hlsli"
-
 VertexShaderOutput main(VertexShaderInput input)
 {
   VertexShaderOutput output;
   output.position = mul(input.position, gTransformationMatrix.wvp);
+  output.texcoord = input.texcoord;
   return output;
 }
 
--- Object3d.hlsli
+++ Object3d.hlsli
@@ -1,4 +1,11 @@
 struct VertexShaderInput
 {
   float32_t4 position : POSITION0;
+  float32_t2 texcoord : TEXCOORD0;
+};
+
+struct VertexShaderOutput
+{
+  float32_t4 position : SV_POSITION;
+  float32_t2 texcoord : TEXCOORD0;
 };
